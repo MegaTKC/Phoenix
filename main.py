d45ca60debcbf7751ac09d0b0aad5eee39e1be24
MegaTKC <t.chun11@outlook.com>
2022-06-22 17:16:16 -0700

    Rename Some variables or whatever you call it

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fileinput import close
-import tkinter as tk
+import tkinter as tk 
 from turtle import window_width
 import webbrowser
 
-my_w = tk.Tk()
-my_w.geometry("500x500")  # Size of the window 
-my_w.title("Phoenix")  # Adding a title
+root = tk.Tk()
+root.geometry("500x500")  # Size of the window 
+root.title("Phoenix")  # Adding a title
 
 my_str = tk.StringVar()
-l1 = tk.Label(my_w,  textvariable=my_str, width=10 )
+l1 = tk.Label(root,  textvariable=my_str, width=10 )
 l1.grid(row=0,column=1,columnspan=5) 
 
 def show_lan(selected_game):
     my_str.set("Launched")
     def create_window():
         if selected_game == "Quit":
-            my_w.quit()
+            root.quit()
         else:
             webbrowser.open(selected_game)
     create_window()
@@ -25,8 +25,8 @@ var = 0
 
 
 for game in gamelist:
-    btn = tk.Button(my_w, text=game, command=lambda lan=game:show_lan(lan))
+    btn = tk.Button(root, text=game, command=lambda lan=game:show_lan(lan))
     btn.grid(row=1,column=var)
     var += 1
 
-my_w.mainloop()
+root.mainloop()
